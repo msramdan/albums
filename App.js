d1b75fcf9c4aeaa8e1dc8170caff26f32c283a34
msramdan <saepulramdan244@gmail.com>
2021-09-29 22:33:16 +0700

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TextInput, Image } from "react-native";
+import HomeComponent from "./components/HomeComponent";
 
 export default function App() {
+  const [testing, setTesting] = useState();
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <HomeComponent testing="Masukan Nama" />
+
+      <TextInput
+        style={{ height: 40, width: 300, borderColor: "gray", borderWidth: 1 }}
+        onChangeText={(text) => setTesting(text)}
+      />
+      <Text>{testing}</Text>
+      <Image style={{ height:100, width:100 }} source={{uri:"https://www.kodingindonesia.com/wp-content/uploads/2017/07/Props-dan-State-di-React-Native.jpg"}} />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +24,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ components/HomeComponent.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {View, Text} from 'react-native'
+
+const HomeComponent = (props) => {
+    return (
+        <View>
+            <Text>{props.testing}</Text>            
+        </View>
+    )
+}
+
+export default HomeComponent
